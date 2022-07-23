cb631aa593f547fd6ba9dad1843da707c5ccad0d
Evelyn Gordon <evelyn.gordon20@gmail.com>
2022-07-23 16:39:16 +0000

    Auto download tests

--- tests/test.js
+++ tests/test.js
@@ -1,8 +1,19 @@
 import JSZip from "jszip";
+import { exec as _exec } from "node:child_process";
+import { existsSync } from "node:fs";
 import { readFile, writeFile } from "node:fs/promises";
+import { promisify } from "node:util";
 import { loadStoryboard } from "../dist/index.js";
+const exec = promisify(_exec);
 
-async function testMap(oszFile, difficulty) {
+async function testMap(oszFile, id, difficulty) {
+  if (!existsSync(oszFile)) {
+    console.log("Downloading", id);
+    console.log(
+      await (await exec(`wget "https://kitsu.moe/api/d/${id}" -O"${oszFile}"`)).stdout
+    );
+  }
+
   const osz = await readFile(oszFile);
   const zip = await JSZip.loadAsync(osz);
 
@@ -24,15 +35,16 @@ async function testMap(oszFile, difficulty) {
   const startTime = performance.now();
   const storyboard = loadStoryboard(osuString, osbString);
   const elapsed = performance.now() - startTime;
-  console.log(`Processed in ${elapsed} ms`);
+  console.log(`Processed in ${elapsed.toFixed(2)} ms`);
 
   await writeFile(
     `${oszFile} ${difficulty}.json`,
     JSON.stringify(storyboard, null, 2)
   );
 }
-await testMap("Panda Eyes - ILY.osz", "[Light Insane]");
+await testMap("Panda Eyes - ILY.osz", 653534, "[Light Insane]");
 await testMap(
   "Oedo Controller (feat. TORIENA) - yunomi.osz",
+  759903,
   "[Enkrypteve's Advanced]"
 );
